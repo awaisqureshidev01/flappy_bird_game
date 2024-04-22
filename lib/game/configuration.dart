2dcc453022c86292ec56bea5017d0e4291da1d09
awaisqureshidev01 <awaisqureshidev01@gmail.com>
2024-04-22 16:57:00 +0500

    Initial commit

---
+++ lib/game/configuration.dart
@@ -0,0 +1,8 @@
+class Config {
+  static const groundHeight = 110.0;
+  static const gameSpeed = 160.0;
+  static const pipeInterval = 1.7;
+  static const birdVelocity = 170;
+  static const gravity = -100.0;
+  static const cloudsHeight = 70.0;
+}
